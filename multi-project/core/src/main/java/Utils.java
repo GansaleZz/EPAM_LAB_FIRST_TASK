04fb4fe491fd31146e0fb40b267daef0549b653a
GansaleZz <72818524+GansaleZz@users.noreply.github.com>
2021-08-10 19:10:48 +0300

    Update Utils.java

--- multi-project/core/src/main/java/Utils.java
+++ multi-project/core/src/main/java/Utils.java
@@ -4,8 +4,4 @@ public class Utils {
     public static boolean isAllPositiveNumbers(String... str){
         return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
     }
-
-    public static void main(String[] args) {
-        Utils.isAllPositiveNumbers("12", "79");
-    }
 }
